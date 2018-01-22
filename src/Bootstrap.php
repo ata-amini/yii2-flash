e6947de3b5f7604d518d0080b77d0c373ca8bdbb
Ata amini <32206135+wyno@users.noreply.github.com>
2018-01-22 19:33:22 +0330

    Add files via upload
    Change directories structure

---
+++ src/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * Author: Ata amini
+     * Email: ata.aminie@gmail.com
+     * Date: 23/09/2017
+     */
+
+    namespace Wyno\Flash;
+
+
+    use yii\base\Application;
+    use yii\base\BootstrapInterface;
+
+    class Bootstrap implements BootstrapInterface
+    {
+
+        /**
+         * Bootstrap method to be called during application bootstrap stage.
+         *
+         * @param Application $app the application currently running
+         */
+        public function bootstrap($app)
+        {
+            \Yii::$container->set('Wyno\Flash\contracts\FlashSessionStore', 'Wyno\Flash\Store');
+
+            // set as singleton
+            \Yii::$container->setSingleton('Wyno\Flash\FlashNotifier');
+        }
+    }
